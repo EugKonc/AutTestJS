022e13ce2a8b26a5299593852a2c3128483a33fb
Eugeni Koncevich <grdnek35@gmail.com>
2021-09-05 19:28:21 +0200

    module_3_tasks_1_2_3_4

---
+++ index.js
@@ -0,0 +1,23 @@
+//adding different types
+console.log("2" + true);
+console.log("2" + 3);
+console.log(3 + true);
+//adding different types
+
+//multiplication different types
+console.log("2" * true);
+console.log("2" * 3);
+console.log(3 * true);
+//multiplication different types
+
+//division different types
+console.log("2" / true);
+console.log("2" / 3);
+console.log(3 / true);
+//division different types
+
+//explicit conversion
+console.log(Number("123"));
+console.log(String(555));
+console.log(Boolean("2"));
+//explicit conversion
