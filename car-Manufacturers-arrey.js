9ae63b56319bc59fb6e687a087707a929ee6cdbf
Eugeni Koncevich <grdnek35@gmail.com>
2021-09-08 14:24:01 +0200

    add car manufacturers array

---
+++ car-Manufacturers-arrey.js
@@ -0,0 +1,10 @@
+//input arrey
+let brand = ["Volkswagen", "Fiat", "GM", "Nissan", ];
+
+//arrey to line and output
+let arrtoStr = brand.toString();
+console.log(arrtoStr);
+
+//line to arrey and output
+let strtoarr = arrtoStr.split(",");
+console.log(strtoarr);
