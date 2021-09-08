1164bf314089385ddb7afc597989a34651022b29
Eugeni Koncevich <grdnek35@gmail.com>
2021-09-08 15:47:37 +0200

    add array to boolean

--- car-Manufacturers-array.js
+++ car-Manufacturers-array.js
@@ -1,10 +1,10 @@
-//input arrey
+//input array
 let brand = ["Volkswagen", "Fiat", "GM", "Nissan", ];
 
-//arrey to line and output
+//array to line and output
 let arrtoStr = brand.toString();
 console.log(arrtoStr);
 
-//line to arrey and output
+//line to array and output
 let strtoarr = arrtoStr.split(",");
 console.log(strtoarr);
